236c61e4c087e4834787d86fab75056fbdf8d219
Finlay Maguire <finlaymaguire@gmail.com>
2015-02-24 17:08:35 +0000

    Modify start script so it is sourced instead of run

--- start_script.sh
+++ start_script.sh
@@ -1,41 +1,31 @@
 #!/bin/bash
-#execute it by typing 
-#. ./start_script
+# execute it by typing 
+# source start_script
 # Courtesy of Krzysztof Geras
 # Modified by Gavin Gray
 # UI improved by Finlay Maguire
 
-usage(){ echo "./start_script.sh -c GPU_CORES"; }
-while getopts "hc:" OPTION; do
-    case $OPTION in
-        h) 
-            usage
-            exit 1
-            ;;
-        c) gpu_number=$OPTARG
-            ;;
-        *)
-            usage
-            exit 1
-            ;;
-    esac
-done
+usage_1(){ echo "./start_script.sh GPU_CORES"; }
+
+gpu_number=$1
 
 if [ -z "${gpu_number##*[!0-9]*}" ]
 then
-    usage
-    exit 1
+    usage_1
+    return 1
 fi
 
 hostname=`hostname`
 
 if [ $hostname = "stonesoup.inf.ed.ac.uk" ]; then
     echo "detected stonesoup, applying config"
-	gpu_number=0
-	export LD_LIBRARY_PATH="/opt/cuda-5.0.35/lib;/opt/cuda-5.0.35/lib64"
+	export LD_LIBRARY_PATH="/opt/cuda-5.0.35/lib:/opt/cuda-5.0.35/lib64"
 	export CUDA_ROOT="/opt/cuda-5.0.35"
 	export THEANO_FLAGS="device=gpu`echo -n $gpu_number`,floatX=float32,base_compiledir=~/.theano/stonesoup`echo -n $gpu_number`"
 else
     echo "not stonesoup, applying default config"
 	export THEANO_FLAGS="device=cpu,floatX=float32,base_compiledir=~/.theano/`echo -n $hostname`/`mktemp -u tmp.XXXXXXXXXXXXXXXXXXXXXXXXXXXXXX`"
 fi
+
+# undef gpu_number
+unset gpu_number
